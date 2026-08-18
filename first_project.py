47ab35de45edc74a8c2616abff485923fa516b70
pedrolikespizza <taiwo.bakare@ucas-edu.net>
2026-08-18 09:41:14 -0600

    Update first_project.py

--- first_project.py
+++ first_project.py
@@ -3,12 +3,3 @@
 print("Hello World")
 print("this is programming 1")
 print("as usual we are experiencing technical difficulties")
-
-# input
-name = input("what is your name: ")
-
-print("welcome to class"  + name)
-
-print("what did you do this + weekend +: ")
-"weekend = input"()
-print("that is super cool i wish i " + "weekend" + " this + weekend +")
