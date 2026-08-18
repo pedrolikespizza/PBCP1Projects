c6869f17f2c6fb9a6916f33017f42edee3e82d13
pedrolikespizza <taiwo.bakare@ucas-edu.net>
2026-08-18 09:32:59 -0600

    firstcommit

---
+++ first_project.py
@@ -0,0 +1,14 @@
+# Pedro Bakare, First Project
+
+print("Hello World")
+print("this is programming 1")
+print("as usual we are experiencing technical difficulties")
+
+# input
+name = input("what is your name: ")
+
+print("welcome to class"  + name)
+
+print("what did you do this weekend: ")
+"weekend = input"()
+print("that is super cool i wish i " + weekend + " this weekend")
