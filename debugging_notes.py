516b3a9ef1418b23d040e2a71ca1aa97e67394ac
pedrolikespizza <taiwo.bakare@ucas-edu.net>
2026-08-21 09:51:46 -0600

    debugging notes

---
+++ debugging_notes.py
@@ -0,0 +1,29 @@
+# pedro bakare debugging notes
+
+
+# syntax error
+print("hello)
+  # indentation error
+if true:
+print("this is true") # <= identation error
+
+people = 10
+print(poeple)
+
+#logic error
+apples = 20
+people = 3
+
+print(apples * people )
+
+#run time errors
+while True:
+   try:
+ fav_num = int(input ("what is your favorite number"))
+except:
+      print("thats not a number")
+      else:
+        break
+        
+
+        print(4+ fav_num)
