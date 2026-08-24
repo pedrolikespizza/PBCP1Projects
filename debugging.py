4da611070958b27f4fd27117030018d95a43a73c
pedrolikespizza <taiwo.bakare@ucas-edu.net>
2026-08-24 09:19:20 -0600

    debugging project changes

---
+++ debugging.py
@@ -0,0 +1,11 @@
+# Pedro Bakare, debugging project 
+name = "Alex"
+age = 16
+
+print("My name is " + name)
+print("And I am",age )
+
+      
+
+# the first mistake with this code was the name not being fully spelt, and it was inbetween two plus signs. the second mistake was the variable not being on the other side of the code
+
