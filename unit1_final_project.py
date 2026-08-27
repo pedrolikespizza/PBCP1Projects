cecb3d25f2d3f0076b56019a4a9832f311d4120e
pedrolikespizza <taiwo.bakare@ucas-edu.net>
2026-08-27 09:03:13 -0600

    final project

---
+++ unit1_final_project.py
@@ -0,0 +1,17 @@
+# pedro bakare unit 1 final test
+name = "Will"
+
+fruit = "banana"
+
+age = 15
+
+sport = "baseball"
+
+input(" hey, what is your name? " + name ) 
+
+input(" that is a wonderful name. what is your favoritve fruit? "  + fruit)
+
+input("wow really! i love bananas too. how old are you?", age )
+(" wow so you're close to getting your drivers permit. do you like any sports? " + sport)
+
+(" that's nice, i dont really like baseball though.")
