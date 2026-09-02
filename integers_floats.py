94bb5dcb76b72a27e22647f11b6fd5cd51b6ea91
pedrolikespizza <taiwo.bakare@ucas-edu.net>
2026-09-02 09:52:08 -0600

    strings notes

---
+++ integers_floats.py
@@ -0,0 +1,31 @@
+# integers and floats notes, PB
+
+#integer is a whole number
+
+num= 75 # just write the number no extra sytanx
+# float # a number with a decimal point.
+pi=3.1415
+
+# arithmetic operators
+
+# arithmetic operators (+ - * / ** //  %)
+
+print(5//2) # integer division ( it only gives you the integer)
+print(5/2) # normal division will always give you a float
+
+# modulo = %
+print(5%2)
+print(10%4)
+print (15%5)
+# order of operations (PEMMDAS) we move from left to right
+
+# assignment operator is the equal sign =
+print(f"Before{num}")
+num=num +2
+
+print(num)
+fav = float(input("what is your favorite umber: "))
+
+print(f"{float(fav)**2} is {fav} squared!")
+print(round(pi))
+print(int(pi))
