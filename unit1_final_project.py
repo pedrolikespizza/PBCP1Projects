9c4379605284b33d3d9ab47bd376f56a305911ae
pedrolikespizza <taiwo.bakare@ucas-edu.net>
2026-09-02 18:24:14 +0000

    changes

--- unit1_final_project.py
+++ unit1_final_project.py
@@ -1,5 +1,17 @@
 # pedro bakare unit 1 final test
 name = input("what is your name:")
 
-print("welcome "+ name)
+print("hello "+ name)
+age = input("how old are you?")
+
+country = input("where are you from?" )
+
  
+print("wow, i've always wanted to go there")
+
+fruit = input("do you like any fruits?")
+
+print("oh, i dont really like those")
+
+
+
