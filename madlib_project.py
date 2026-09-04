d3ff71bf29d3c6ffd8030053b59b15c460964ba0
pedrolikespizza <taiwo.bakare@ucas-edu.net>
2026-09-04 18:24:58 +0000

    message

--- madlib_project.py
+++ madlib_project.py
@@ -1,1 +1,15 @@
-# pedro bakare madlib project
+#pedro bakare madlib project
+name = input("Enter a person's name: ")
+animal = input("Enter an animal: ")
+place = input("Enter a place: ")
+adjective = input("Enter an adjective: ")
+verb = input("Enter a verb: ")
+food = input("Enter a food: ")
+
+story = "One day, " + name + " went to " + place + " with a " + animal + ". "
+story = story + "The " + animal + " was really " + adjective + " and started to " + verb + " around. "
+story = story + name + " got hungry and pulled out some " + food + ". "
+story = story + "The " + animal + " quickly grabbed the " + food + " and ran away. "
+story = story + "Everyone at " + place + " laughed as " + name + " chased after the " + animal + "."
+
+print(story)
