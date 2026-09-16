c5318c61c5cca620426243f3170c39a02cd569cf
pedrolikespizza <taiwo.bakare@ucas-edu.net>
2026-09-16 09:53:01 -0600

    debugger stuff

---
+++ using_debugger_.py
@@ -0,0 +1,11 @@
+print(f"the average grade is {average}")
+
+scores= [12, 45, 7, 68, 33, 90, 21]
+
+running_total = 0
+highest_score = 0
+for score in scores:
+    highest_score=score
+
+print(f"Total:{running_total}")
+print(f"highest score: {highest_score}")    
---
+++ using_debugger_notes.py
@@ -0,0 +1,28 @@
+# pedro bakare 
+# a debugger already in most IDE's
+# shift+F5 reset resets the debugger
+#shift+F11 step out takes us out of a function
+#F11 step in takes us into a function
+#F5 steps over the 
+#just runs the code until the breakpoint
+'''grades = [85, 90, 78, 92, 88]
+
+total = 0
+count = len(grades)
+
+for grade in grades:
+    total = total + grade
+
+average = total / count
+
+print(f"the average grade is {average}")
+'''
+scores= [12, 45, 7, 68, 33, 90, 21]
+
+running_total = 0
+highest_score = 0
+for score in scores:
+    highest_score=score
+
+print(f"Total:{running_total}")
+print(f"highest score: {highest_score}")    
