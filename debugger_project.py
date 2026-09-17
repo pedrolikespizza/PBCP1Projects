29ac26e11f82d99646246e75a9980a90e1f77695
pedrolikespizza <taiwo.bakare@ucas-edu.net>
2026-09-17 09:35:08 -0600

    67 changes on debugger

--- debugger_project.py
+++ debugger_project.py
@@ -1,4 +1,4 @@
-# pedro bakare
+# pedro bakare debugging project
 
 # Ravager Snack Bar
 import random
@@ -7,17 +7,17 @@ pirate_name = input("What's your name, pirate? ")
 snack_name = input("What snack do you want? ")
 
 price = random.randint(2, 8)  # random price in credits
-quantity = input("How many would you like? ")
+quantity = int(input("How many would you like? "))
 
 total = price * quantity
 
-discounted_total = total - 2 * 0.10
+discounted_total = total * 0.9
 
 tax_rate = 0.08
-total_with_tax = discounted_total + (discounted_total * tax_rate)
+total_with_tax = discounted_total + (total * tax_rate)
 
 print("Hello, " + pirate_name + "! Here's your order summary:")
 print("Snack: " + snack_name)
 print("Price per snack: " + str(price) + " credits")
-print("Total before tax: " + str(price))
+print("Total before tax: " + str(total))
 print("Total with tax: " + str(round(total_with_tax, 2)) )
