44a8ac0623ec04fd156bcc2a066a23262cbb82fc
pedrolikespizza <taiwo.bakare@ucas-edu.net>
2026-09-18 09:31:54 -0600

    average grades done

--- averagegrades_project.py
+++ averagegrades_project.py
@@ -1,11 +1,11 @@
 # pedro bakare, average grades project
-period_1 = input("enter grade for programming: " )
-period_2 = input("enter grade for biology: ")
-period_3 = input("enter grade for geography: ")
-period_4 = input("enter grade for advisory: ")
-period_5 = input("enter grade for english: ")
-period_6 = input("enter grade for drawing: ")
-period_7 = input("enter grade for math: ")
+period_1 = float(input("enter grade for programming: " ))
+period_2 = float(input("enter grade for biology: "))
+period_3 = float(input("enter grade for geography: "))
+period_4 = float(input("enter grade for advisory: "))
+period_5 = float(input("enter grade for english: "))
+period_6 = float(input("enter grade for drawing: "))
+period_7 = float(input("enter grade for math: "))
 
 total= period_1 + period_2 + period_3 + period_4 + period_5 + period_6 + period_7
 average = total / 7
